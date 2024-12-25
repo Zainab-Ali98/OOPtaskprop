66369813cc7a8df4505258c9586f64a1213034fa
zeeen <zeeen@DESKTOP-GP9JGLO>
2024-12-25 12:16:38 +0300

    Add project files.

---
+++ OOPtaskprop/Models/Users.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtaskprop.Models
+{
+    public class Users
+    {   
+        string name;
+        public string UserName
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("please enter user name");
+                   
+                }
+                else
+                {
+                    name = value;
+                }
+
+            }
+        }
+        double Salary;
+        public double salary
+        { 
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                if(value >=350 && value <= 900)
+                {
+                    Salary = value;
+
+                    Console.WriteLine($"Salary is : {salary}");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("salary is our of range");
+                }
+            }
+
+        }
+
+        public void GetUserName()
+        {
+            Console.WriteLine("User name is :" + name);
+        }
+
+    }
+}
---
+++ OOPtaskprop/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOPtaskprop.Models;
+
+Users user = new Users();
+
+Console.WriteLine("enter your name");
+user.UserName = Console.ReadLine();
+user.GetUserName();
+
+Console.WriteLine("enter salary");
+user.salary =Convert.ToDouble(Console.ReadLine());
